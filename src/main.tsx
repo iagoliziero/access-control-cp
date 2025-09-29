85efc0712695711701e7b21eabb2eb0a7d94c1d1
iagoliziero <iagolizierox14@gmail.com>
2025-09-29 09:30:53 -0300

    feat: created routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Cadastro from './routes/cadastro'
+import Login from './routes/login'
+import Layout from './component/Layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

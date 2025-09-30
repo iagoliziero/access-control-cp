5a88e7cff9f30bce7633114daf4ffe576740d7f9
enricodelesporte <enricodelesporte@hotmail.com>
2025-09-30 09:53:26 -0300

    feat: add validation errors

--- src/routes/cadastro/index.tsx
+++ src/routes/cadastro/index.tsx
@@ -1,8 +1,17 @@
 import { Mail, User, UsersRound } from "lucide-react";
 import Input from "../../component/Input";
 import Button from "../../component/Button";
+import { useForm } from "react-hook-form";
+import type { IDataForm } from "../../interfaces/IdataForm";
 
 const Cadastro = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors}
+    } = useForm<IDataForm>();
+
   return (
     <section className="section">
       <div>
@@ -11,32 +20,47 @@ const Cadastro = () => {
       <form className="card">
         <div>
           <Input
+            register={register}
+            rules={{required: true}}
             id="name"
             name="name"
             placeholder="Digite Seu name"
             label="Name *"
             icon={<UsersRound className="text-white/70" />}
           />
+          {errors?.name?.type === 'required' && (
+            <p className="p-errors__input">Nome é obrigatório.</p>
+          )}
         </div>
         <div>
           <Input
+            register={register}
+            rules={{required: true}}
             id="username"
             name="username"
             placeholder="Digite Seu Username"
             label="Username *"
             icon={<User className="text-white/70" />}
           />
+          {errors?.username?.type === 'required' && (
+            <p className="p-errors__input">Nome do usuário é obrigatório.</p>
+          )}
         </div>
         <div>
           <Input
+            register={register}
+            rules={{required: true}}
             id="email"
             name="email"
             placeholder="Digite Seu Email"
             label="Email *"
             icon={<Mail className="text-white/70" />}
           />
+          {errors?.email?.type === 'required' && (
+            <p className="p-errors__input">Email é obrigatório.</p>
+          )}
         </div>
-        <Button onClick={function(){alert("Funciono")}}>
+        <Button type="submit" onClick={function(){alert("Funciono")}}>
             Cadastrar
         </Button>
       </form>

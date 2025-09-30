344bb0a4d6e0c2a63f945e43790d9edce7013918
enricodelesporte <enricodelesporte@hotmail.com>
2025-09-30 08:57:59 -0300

    feat: adding inputs from form

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 export default function Layout() {
 
   return (
-    <div className="w-full h-screen p-5 items-center flex flex-col justify-between bg-[url('public/background-forest.png')] bg-cover bg-center bg-no-repeat bg-black/25 bg-blend-multiply">
+    <div className="w-full h-screen p-5 items-center flex flex-col justify-between bg-[url('background-forest.png')] bg-cover bg-center bg-no-repeat bg-black/25 bg-blend-multiply">
       <header>
         <Header />
       </header>
--- src/routes/cadastro/index.tsx
+++ src/routes/cadastro/index.tsx
@@ -1,7 +1,34 @@
+import { Mail, User } from "lucide-react";
+import Input from "../../component/Input";
+
 const Cadastro = () => {
-    return ( 
-        <div> Cadastro</div>
-     );
-}
- 
+  return (
+    <section className="section">
+      <div>
+        <h1 className="title">Cadastrar Usuário</h1>
+      </div>
+      <form className="card">
+        <div>
+          <Input
+            id="username"
+            name="username"
+            placeholder="Digite Seu Username"
+            label="Username *"
+            icon={<User className="text-white/70" />}
+          />
+        </div>
+        <div>
+          <Input
+            id="email"
+            name="email"
+            placeholder="Digite Seu Email"
+            label="Email *"
+            icon={<Mail className="text-white/70" />}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
 export default Cadastro;

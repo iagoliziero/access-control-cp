9fb1678a9f067a26872fc7b351459de19e1aa942
vitordias1006 <vitordias09532@gmail.com>
2025-10-02 09:49:48 -0300

    feat: add arrow on button component

--- src/component/Button.tsx
+++ src/component/Button.tsx
@@ -3,7 +3,7 @@ import type { IButton } from "../interfaces/IButton";
 const Button = ({children, onClick, type}: IButton) => {
     return (
         <button type={type} onClick={onClick} className="button__card">
-            {children}
+            {children} →
         </button>
     );
 }
